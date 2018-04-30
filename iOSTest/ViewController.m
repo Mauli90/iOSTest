e11441bddc2b8d21ebc1794b6235feae0747bf57
d.wakchaure <dnyaneshwar.wakchaure@ignitesol.com>
2018-04-30 22:19:05 +0530

    View controller with tableView

--- iOSTest/ViewController.m
+++ iOSTest/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "CustomTableViewCell.h"
 
 @interface ViewController ()
 
@@ -16,9 +17,53 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    array = [[NSArray alloc] init];
+    [self plotUI];
+
     // Do any additional setup after loading the view, typically from a nib.
 }
+// Programatically UI plot
+- (void)plotUI{
+    int width = [[UIScreen mainScreen] bounds].size.width;
+    int height = [[UIScreen mainScreen] bounds].size.height;
+    
+    _tableView = [[UITableView alloc]init];
+    _tableView.frame = CGRectMake(0,20,width,height);
+    _tableView.dataSource = self;
+    _tableView.delegate = self;
+    [_tableView registerClass:[CustomTableViewCell class] forCellReuseIdentifier:@"Cell"];
+    _tableView.autoresizingMask = UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleWidth;
+    [_tableView reloadData];
+    [self.view addSubview:_tableView];
+   
+}
 
+
+
+// Table view Delegate & Datasource Methods
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return array.count;
+}
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
+    return 1;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section
+{
+    return 30;
+}
+
+-(CGFloat)tableView:(UITableView *)tableView estimatedHeightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return 100;
+}
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    CustomTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell" forIndexPath:indexPath];
+    
+    return cell;
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
