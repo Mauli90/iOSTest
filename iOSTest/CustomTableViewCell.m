d6300eeffee6a7dd012c1c201327c38332eaf5c0
d.wakchaure <dnyaneshwar.wakchaure@ignitesol.com>
2018-05-01 01:00:48 +0530

    fixes contraint

--- iOSTest/CustomTableViewCell.m
+++ iOSTest/CustomTableViewCell.m
@@ -7,6 +7,7 @@
 //
 
 #import "CustomTableViewCell.h"
+#import <Masonry/Masonry.h>
 
 @implementation CustomTableViewCell
 
@@ -23,10 +24,9 @@
 - (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier {
     self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
     if (self) {
-        int width = [[UIScreen mainScreen] bounds].size.width;
         
         _imgView = [[UIImageView alloc] init];
-        _imgView.contentMode = UIViewContentModeScaleAspectFit;
+        _imgView.contentMode = UIViewContentModeScaleToFill;
         [_imgView setTranslatesAutoresizingMaskIntoConstraints:NO];
         
         [self.contentView addSubview:_imgView];
@@ -43,6 +43,35 @@
         [_descLabel setFont:[UIFont fontWithName:@"HelveticaNeue" size:18.0f]];
         [_descLabel setTranslatesAutoresizingMaskIntoConstraints:NO];
         [self.contentView addSubview:_descLabel];
+        
+        UIEdgeInsets padding = UIEdgeInsetsMake(0, 0, 0, 0);
+        
+        [_imgView mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.width.equalTo(@(100));
+            make.height.equalTo(@(100));
+           make.top.equalTo(self.contentView.mas_top).with.offset(padding.top);
+            make.left.equalTo(self.contentView.mas_left).with.offset(padding.left);
+            make.bottom.equalTo(self.contentView.mas_bottom).with.offset(-padding.bottom).priorityMedium();
+        }];
+        
+        UIEdgeInsets padding1 = UIEdgeInsetsMake(5, 10, 10, 10);
+        
+        [_titleLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+          make.top.equalTo(self.contentView.mas_top).with.offset(padding1.top);
+          make.left.equalTo(_imgView.mas_right).with.offset(padding1.left);
+            make.right.equalTo(self.contentView.mas_right).with.offset(-padding1.right);
+        }];
+        
+        UIEdgeInsets padding2 = UIEdgeInsetsMake(5, 10, 10, 10);
+        
+        [_descLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.top.equalTo(_titleLabel.mas_bottom).with.offset(padding2.top);
+            make.left.equalTo(_imgView.mas_right).with.offset(padding2.left);
+            make.bottom.equalTo(self.contentView.mas_bottom).with.offset(-padding2.bottom);
+            
+            make.right.equalTo(self.contentView.mas_right).with.offset(-padding2.right);
+        }];
+       
         
     }
     return self;
